def3840f7f5cff16ec4a29f66bc7d57201d317ae
Tomasz-Wolszczak <tomasz.wolszczak@gmail.com>
2018-07-24 01:41:16 +0200

    feat: Add grid size selector

--- designs.js
+++ designs.js
@@ -1,5 +1,27 @@
+/*
+There will be a lot of comments in this file. You may find them unnecessary, but I'm still learning and I find them helpful for me.
+*/
+
 // Select color input
-// Select size input
+
+// Grid size
+let gridHeight;
+let gridWidth;
+
+// Get grid size values
+document.querySelector('#sizePicker').addEventListener('submit', function (event) {
+  // Prevent page refreshing
+  event.preventDefault();
+  // TODO: Remove test log
+  console.log(`Test log: submit button clicked`);
+  gridHeight = document.querySelector('#inputHeight').value;
+  // TODO: Remove test log
+  console.log(`Test log: value of variable "gridHeight" is set to ${gridHeight}.`);
+  gridWidth = document.querySelector('#inputWidth').value;
+  // TODO: Remove test log
+  console.log(`Test log: value of variable "gridWidth" is set to ${gridWidth}.`);
+  // TODO: Call makeGrid
+});
 
 // When size is submitted by the user, call makeGrid()
 
