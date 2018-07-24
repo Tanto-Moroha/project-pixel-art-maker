7c19d6534f82a3ee8029a237ad4c9482792028fe
Tomasz-Wolszczak <tomasz.wolszczak@gmail.com>
2018-07-24 03:56:19 +0200

    style: Add minor style changes

--- designs.js
+++ designs.js
@@ -1,13 +1,16 @@
-/*
-There will be a lot of comments in this file. You may find them unnecessary, but I'm still learning and I find them helpful for me.
+/**
+* There will be a lot of comments in this file. You may find them unnecessary, but I'm still learning and I find them helpful for me.
 */
 
-// Set a Color Picker
+// Set objects to read/manipulate
 const colorPicker = document.querySelector('#colorPicker');
 const pixelCanvas = document.querySelector('#pixelCanvas');
+
+
 // Grid size
 let gridHeight;
 let gridWidth;
+
 
 // Get grid size values
 document.querySelector('#sizePicker').addEventListener('submit', function (event) {
@@ -20,31 +23,27 @@ document.querySelector('#sizePicker').addEventListener('submit', function (event
   makeGrid();
 });
 
+
 // Draw a grid
 function makeGrid() {
-
   // Remove previous table if there was any
   while (pixelCanvas.firstChild) {
     pixelCanvas.removeChild(pixelCanvas.firstChild);
   }
-
   // Draw rows and cells
   for (let row = 1; row <= gridHeight; row++) {
-    // Create a new row
     const newRow = document.createElement('tr');
     pixelCanvas.appendChild(newRow);
 
-    for (let col =1; col <= gridWidth; col++) {
-      // Create a new cell
+    for (let col = 1; col <= gridWidth; col++) {
       const newCell = document.createElement('td');
       newRow.appendChild(newCell);
     }
 
   }
-
   pixelCanvas.addEventListener('click', respondToTheClick);
-
 }
+
 
 function respondToTheClick (evt) {
   // Change a cell's color
