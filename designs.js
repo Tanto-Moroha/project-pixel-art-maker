a8f41029d5e3d1b942a588ec5573e9c07643a0f5
Tomasz-Wolszczak <tomasz.wolszczak@gmail.com>
2018-07-24 02:04:19 +0200

    feat: Add grid maker

--- designs.js
+++ designs.js
@@ -4,7 +4,26 @@
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
+  // TODO: Remove test log
+  console.log(`Test log: function makeGrid started.`);
 
-// Your code goes here!
+  for (let row = 1; row <= gridHeight; row++) {
+    // Create a new row
+    const newRow = document.createElement('tr');
+    document.querySelector('#pixelCanvas').appendChild(newRow);
+
+    for (let col =1; col <= gridWidth; col++) {
+      // Create a new cell
+      const newCell = document.createElement('td');
+      newRow.appendChild(newCell);
+    }
+
+  }
 
 }
+
+// TODO: Remove test after branch merging
+// Test for a module
+const gridHeight = 10;
+const gridWidth = 5;
+makeGrid();
