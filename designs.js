0e91c6cd98cc6fa8b6f910b79df7d1d734666744
Tomasz-Wolszczak <tomasz.wolszczak@gmail.com>
2018-07-24 03:07:57 +0200

    feat: Add Color Picker

--- designs.js
+++ designs.js
@@ -1,4 +1,11 @@
 // Select color input
+const colorPicker = document.querySelector('#colorPicker');
+
+// Test for color picker
+document.addEventListener('keypress', function () {
+  console.log(colorPicker.value);
+});
+
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
